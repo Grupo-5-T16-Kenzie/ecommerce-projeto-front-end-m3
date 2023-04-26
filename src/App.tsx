9d660e287b45abc777300d308333235f76b1fb54
wellington-nunes <wellingtonnunez10@gmail.com>
2023-04-26 16:22:42 -0300

    feat: Arquitetura base do projeto.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
+export const App = () => {
  
   return (
-    <>
-    
-    </>
+    <h1>Projeto Final</h1>
   )
 }
 
-export default App
