316ba369fad6ee2f3d1d170a32400f109c8794fe
Eneias Morais <eneiasmorais@gmail.com>
2023-05-02 09:37:10 -0300

    feat: Inicialização de estilização CSS do Desktop e funcionalidades de usuário logado

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { ProductCard } from "./ProductCard/ProductCard";
-import { IProductsContext } from "../Interfaces/Interfaces";
+import { IProductsContext } from "../../Interfaces/Interfaces";
 import { AuthProductsContext } from "../../providers/productsContext";
 import { StyledProductsList } from "./styles";
 
@@ -8,25 +8,25 @@ export const ProductsList = () => {
   const { products, category } =
     useContext<IProductsContext>(AuthProductsContext);
 
-    console.log(products)
-    console.log(category)
-    
+  console.log(products);
+  console.log(category);
 
-    return (
-      <StyledProductsList>
-        {category !== "todos" 
-  ? products.filter(product => product.category.toLowerCase() === category.toLowerCase()).map((filtered) => ( 
-      <ProductCard key={filtered.id} product={filtered} />
-    )) 
-  : products.map((product) => (
-      <ProductCard key={product.id} product={product} />
-    ))
-}
-
-      </StyledProductsList>
-    );
-
-
-
-  };
-
+  return (
+    <StyledProductsList>
+      <ul className="products__list">
+        {category !== "todos"
+          ? products
+              .filter(
+                (product) =>
+                  product.category.toLowerCase() === category.toLowerCase()
+              )
+              .map((filteredProduct) => (
+                <ProductCard key={filteredProduct.id} product={filteredProduct} />
+              ))
+          : products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+          ))}
+      </ul>
+    </StyledProductsList>
+  );
+};
