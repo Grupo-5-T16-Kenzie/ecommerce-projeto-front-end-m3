d74f9c9ac135517713554e14de67e0cfc969ff69
wellington-nunes <wellingtonnunez10@gmail.com>
2023-05-03 16:40:19 -0300

    feat: Adiciona os schemas de validação dos formulários

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,17 +1,15 @@
-import { forwardRef, ForwardedRef } from "react";
+import { ForwardedRef, forwardRef, InputHTMLAttributes } from "react";
 import { FieldError } from "react-hook-form";
 
-interface IIputProps {
-  label?: string;
-  error?: FieldError;
-}
+interface IInputProps extends InputHTMLAttributes<HTMLInputElement>{
+    label?: string;
+    error?: FieldError;
+} 
 
-export const Input = forwardRef(
-  ({ label, error, ...rest }: IIputProps, ref: ForwardedRef<HTMLInputElement>) => (
+export const Input = forwardRef(({label, error, ...rest}: IInputProps, ref: ForwardedRef<HTMLInputElement>) => (
     <div>
-      {label ? <label>{label}</label> : null}
-      <input ref={ref} {...rest} />
-      {error ? <p>{error.message}</p> : null}
+        {label ? <label>{label}</label> : null}
+        <input ref={ref} {...rest} />
+        {error ? <p>{error.message}</p> : null}
     </div>
-  )
-);
+))
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { UserContext } from "../../providers/UserContext";
+import { Input } from "../Input/Input";
 
 export interface ILoginFormData {
   email: string;
@@ -24,8 +25,12 @@ export const LoginForm = () => {
 
   return (
     <form onSubmit={handleSubmit(submit)}>
-      <input placeholder="Email" type="email" {...register("email")} disabled={loading} />
+      <Input type="email" {...register("email")} />
+      <Input type="password" {...register("password")} />
+
+      {/* <input placeholder="Email" type="email" {...register("email")} disabled={loading} />
       <input placeholder="Senha" type="password" {...register("password")} disabled={loading} />
+       */}
       <button type="submit" disabled={loading}>
         {" "}
         {loading ? "ENTRANDO..." : "ENTRAR"}
